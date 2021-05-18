7e9cb2cd123498f68c22d42b1a6d7cba4edb25e1
favor78 <79551589+favor78@users.noreply.github.com>
2021-05-18 01:52:03 +0100

    Update Oreo Token.sol

--- Oreo Token.sol
+++ Oreo Token.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.6.12;
 
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+import "@oreoswap/oreo-swap-lib/contracts/token/BEP20/BEP20.sol";
 
-// CakeToken with Governance.
-contract CakeToken is BEP20('PancakeSwap Token', 'Cake') {
+// OreoToken with Governance.
+contract OreoToken is BEP20('OreoSwap Token', 'Oreo') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
